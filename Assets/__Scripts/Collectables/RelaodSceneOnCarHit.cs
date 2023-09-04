c10d1561737fa99efa2c624f1c0ac6a006f7402b
Simon Tepel <53453001+Enceladus2521@users.noreply.github.com>
2023-09-04 15:57:42 +0200

    PlayerRespawn

---
+++ Assets/__Scripts/Collectables/RelaodSceneOnCarHit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelaodSceneOnCarHit : MonoBehaviour
+{
+   [SerializeField] private GameObject Explosion;
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Car")
+        {
+
+            StartCoroutine(ReloadScene());
+        }
+    }
+
+    IEnumerator ReloadScene()
+    {
+        GameObject explo = Instantiate(Explosion, transform.position, Quaternion.identity);
+        explo.transform.localScale = 5 * Vector3.one;
+
+        yield return new WaitForSeconds(0.5f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
