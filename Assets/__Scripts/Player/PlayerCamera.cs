93bd12e8b82551030d1495a080351f5872a43dd1
Simon Tepel <53453001+Enceladus2521@users.noreply.github.com>
2023-09-05 16:14:47 +0200

    fok

--- Assets/__Scripts/Player/PlayerCamera.cs
+++ Assets/__Scripts/Player/PlayerCamera.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    #region Variables
+    [SerializeField] GameObject defaultCamera;
+
+
+    
+
+
+
+
+
+    #endregion
+
+
+    #region CamCallMethods
+
+
+    #endregion
+
 }
