c188e1284775c544f345b483fd287f4ef69b2181
Paul <pbochman@hs-mittweida.de>
2023-09-06 15:41:37 +0200

    fixed coin pickup issue

--- Assets/__Scripts/UI/UIController.cs
+++ Assets/__Scripts/UI/UIController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreboardUIController : MonoBehaviour
+public class UIController : MonoBehaviour
 {
     
     [SerializeField] private TMPro.TextMeshProUGUI scoreText;
