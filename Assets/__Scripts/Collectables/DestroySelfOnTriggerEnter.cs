99ab2e937d62614d2b1511e25d01054fa57a4294
Paul <pbochman@hs-mittweida.de>
2023-09-09 14:53:28 +0200

    changed coin popup when collecting

--- Assets/__Scripts/Collectables/DestroySelfOnTriggerEnter.cs
+++ Assets/__Scripts/Collectables/DestroySelfOnTriggerEnter.cs
@@ -15,11 +15,6 @@ public class DestroySelfOnTriggerEnter : MonoBehaviour
             //Debug.Log("Collected Coin");
             Scoreboard.Instance.coinCollect();
 
-            //load from the Resources folder
-            GameObject nr = Instantiate(Resources.Load<GameObject>("Nr/FN").gameObject);
-            nr.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-            nr.GetComponent<FlowtNr>().SetTextAndRotation("+1");
-
             Destroy(gameObject);
         }
     }
