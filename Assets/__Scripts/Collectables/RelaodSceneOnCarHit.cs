559caa8f0d0e9d0001d97ea8dd168db468fe629f
Simon Tepel <53453001+Enceladus2521@users.noreply.github.com>
2023-09-11 12:01:02 +0200

    ScreenSchakePlayer

--- Assets/__Scripts/Collectables/RelaodSceneOnCarHit.cs
+++ Assets/__Scripts/Collectables/RelaodSceneOnCarHit.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class RelaodSceneOnCarHit : MonoBehaviour
 {
-   [SerializeField] private GameObject Explosion;
+    [SerializeField] private GameObject Explosion;
     private void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag == "Car")
+        if (other.gameObject.tag == "Car")
         {
 
-            
+
             GameObject explo = Instantiate(Explosion, transform.position, Quaternion.identity);
+
+            CinemachineImpulseSource imp;
+            if (gameObject.AddComponent<CinemachineImpulseSource>() == null)
+            {
+                imp = gameObject.AddComponent<CinemachineImpulseSource>();
+            }
+            else imp = gameObject.GetComponent<CinemachineImpulseSource>();
+            
+
+
+
+            imp.m_ImpulseDefinition.m_ImpulseType = CinemachineImpulseDefinition.ImpulseTypes.Dissipating;
+            imp.m_ImpulseDefinition.m_ImpulseShape = CinemachineImpulseDefinition.ImpulseShapes.Rumble;
+
+            imp.GenerateImpulse();
             explo.transform.localScale = 5 * Vector3.one;
             Destroy(explo, 5);
             GameManager.Instance.playerDeath();
@@ -22,8 +40,8 @@ public class RelaodSceneOnCarHit : MonoBehaviour
 
     IEnumerator ReloadScene()
     {
-        
-        
+
+
 
         yield return new WaitForSeconds(0.5f);
         //Scene scene = SceneManager.GetActiveScene();
